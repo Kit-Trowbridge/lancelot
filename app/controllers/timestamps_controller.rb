83dcb8110cce5e8b738f950370430b40897daee6
Rita Starzyk <rita_s@mail.com>
2025-02-06 15:04:48 +0000

    backend and front end

--- app/controllers/timestamps_controller.rb
+++ app/controllers/timestamps_controller.rb
@@ -1,5 +1,6 @@
 class TimestampsController < ApplicationController
   before_action :set_chained_instances
+
   def show
     @timestamp = Timestamp.find(params[:id])
   end
@@ -10,11 +11,15 @@ class TimestampsController < ApplicationController
 
   def create
     # should stop a running stopwatch when a new one is clicked
-    Timestamp.last.update(end_time: DateTime.now) if Timestamp.last.end_time.nil?
+    if Timestamp.last.present? && Timestamp.last.end_time.nil?
+      Timestamp.last.update(end_time: DateTime.now)
+    end
+
     @timestamp = Timestamp.new
     @timesheet = Timesheet.find(params[:timesheet_id])
     @timestamp.timesheet = @timesheet
     @timestamp.start_time = DateTime.now
+
     if @timestamp.save!
       redirect_to timesheet_timestamp_path(@timesheet, @timestamp)
     else
@@ -26,7 +31,6 @@ class TimestampsController < ApplicationController
     @timestamp = Timestamp.find(params[:id])
     @timestamp.update(end_time: DateTime.now)
     redirect_to new_timesheet_timestamp_path(@timesheet)
-    # redirect_to client_project_timesheet_path(@timesheet.project.client, @timesheet.project, @timesheet)
   end
 
   def edit
@@ -54,7 +58,7 @@ class TimestampsController < ApplicationController
 
   def set_chained_instances
     @timesheet = Timesheet.find(params[:timesheet_id]) if params[:timesheet_id]
-    @project = @timesheet.project
+    @project = @timesheet.project if @timesheet
   end
 
   def timestamp_params
